1e6de7d66e630fbbfad435145aadda63956327ec
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-06-22 22:35:53 -0500

    Update unloadext.js

--- unloadext.js
+++ unloadext.js
@@ -4,8 +4,8 @@
   if (!Scratch.extensions.unsandboxed) {
     throw new Error('This Extension Must Run Unsandboxed');
   }
-  const vm = Scratch.vm
-  window.onbeforeunload = function(){vm.runtime.startHats("unload_onunload")}
+  const vm = Scratch.vm;
+  window.onbeforeunload = function(){vm.runtime.startHats("unload_onunload")};
 
   class ext {
     getInfo() {
@@ -20,6 +20,7 @@
           }
         ]
       };
+      onunload(args,util){}
     }
   Scratch.extensions.register(new ext());
 })(Scratch);
